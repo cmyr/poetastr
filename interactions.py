719eff39762e353df15fc8f4f4c87c027bd625cd
cmyr <colin@cmyr.net>
2015-05-23 11:39:01 -0400

    added interactions.py for handling twitter stuff

---
+++ interactions.py
@@ -0,0 +1,76 @@
+# coding: utf-8
+
+"""
+handles interactions with the twitter API.
+"""
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+import json
+import functools
+import re
+import os
+from twitter.api import Twitter, TwitterError, TwitterHTTPError
+from twitter.oauth import OAuth
+
+import zmqstream
+from twittercreds import (CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+
+class TwitterHandler(object):
+    """wraps twitter API calls and tracks related state"""
+    def __init__(self):
+        super(TwitterHandler, self).__init__()
+        self.api = self.load_twitter()     
+        self.last_mention = None
+        # self.stream = self.load_stream()
+    
+    def load_twitter(self):
+        return Twitter(
+        auth=OAuth(ACCESS_KEY, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET),
+        api_version='1.1'
+        )
+
+    def fetch_posts(self, user_name, count=200):
+        max_items = 200
+        tweets = list()
+
+        params = {'screen_name': user_name, 'trim_user': 1, 'count': min(max_items, count)}
+        posts = [None, None] # we get an array of length 1 when we're out of entries
+        while len(posts) > 1:
+            posts = self.api.statuses.user_timeline(**params)
+            next_id = min([int(p.get('id_str')) for p in posts])
+            params['max_id'] = next_id
+            tweets.extend(posts)
+        for t in [p.get('text') for p in tweets]:
+            print(t)
+
+    def new_mentions(self):
+        params = {'count': 200}
+        if self.last_mention:
+            params['since_id'] = self.last_mention
+        mentions = self.api.statuses.mentions_timeline(**params)
+        if len(mentions):
+            self.last_mention = max(int(m.get('id_str')) for m in mentions)
+        users = [m.get('entities').get('user_mentions') for m in mentions]
+        users = [m for m in users if len(m)]
+        print(users)
+
+
+
+
+def main():
+    pass
+    # return fetch_posts('cmyr')
+    # import argparse
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-o', '--outpath', type=str, default=output_url, help="set outpath")
+    # parser.add_argument('-c', '--count', type=int, default=20, help='number of items to save')
+    # args = parser.parse_args()
+    # fetch_recent_posts(**vars(args))
+
+
+
+
+if __name__ == "__main__":
+    main()
